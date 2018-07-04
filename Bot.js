878475d2551e943a39f51dd3f9b95675f202836a
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-04 03:15:29 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -7,7 +7,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-	client.user.setActivity(`le!help (${client.guilds.size} servers)!`, {type: 'Watching'});
+	client.user.setActivity(`${client.guilds.size} servers! le!help`, {type: 'Watching'});
 	//const channel = client.channels.get('458252935172849688');
 	//if (!channel) return;
 	//channel.send("Hi");
